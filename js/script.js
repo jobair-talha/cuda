04d150f37c3bb1640098a53774a876fe46363533
jobair-talha <78406840+jobair-talha@users.noreply.github.com>
2021-03-06 09:33:00 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$('.fst-prg').percentageLoader({
+    valElement: 'p',
+    strokeWidth: 15,
+    bgColor: '#d9d9d9',
+    ringColor: '#30bae7',
+    textColor: '#9a9a9a',
+    fontSize: '12px',
+    fontWeight: 'normal'
+  });
+  $('.scnd-prg').percentageLoader({
+    valElement: 'p',
+    strokeWidth: 15,
+    bgColor: '#d9d9d9',
+    ringColor: '#d74680',
+    textColor: '#9a9a9a',
+    fontSize: '12px',
+    fontWeight: 'normal'
+  });
+  $('.thrd-prg').percentageLoader({
+    valElement: 'p',
+    strokeWidth: 15,
+    bgColor: '#d9d9d9',
+    ringColor: '#15c7a8',
+    textColor: '#9a9a9a',
+    fontSize: '12px',
+    fontWeight: 'normal'
+  });
+  $('.fth-prg').percentageLoader({
+    valElement: 'p',
+    strokeWidth: 15,
+    bgColor: '#d9d9d9',
+    ringColor: '#eb7d4b',
+    textColor: '#9a9a9a',
+    fontSize: '12px',
+    fontWeight: 'normal'
+  });
+  var mixer = mixitup('.main');
+  
